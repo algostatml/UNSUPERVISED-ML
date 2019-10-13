f7aca154eabc053518ffa754587d279a38972517
kennedy <kennyjunior022006@yahoo.com>
2019-10-14 01:46:01 +0200

    Classical KMeans
    Classical KMeans

---
+++ KMEANS AND KERNEL VERSION/kmeans.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 14 01:40:59 2019
+
+@author: kenneth
+"""
+
+from __future__ import absolute_import
+import numpy as np
+
+
+class kmeans:
+    def __init__(self):
+        '''
+        '''
+        return
+    
+    def fit(self):
+        '''
+        '''
+        return
