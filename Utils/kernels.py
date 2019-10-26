3cb19e869e86ad4d7a34481985d767d9d3db507c
kennedy <kennyjunior022006@yahoo.com>
2019-10-26 18:04:42 +0200

    Updated kernels
    Updated kernels

--- Utils/kernels.py
+++ Utils/kernels.py
@@ -134,7 +134,7 @@ class Kernels:
         else:
             gamma = gamma
         if not op:
-            op = 'add' #add seems like the best performning here
+            op = 'dot' #add seems like the best performning here
         else:
             op = op
         if op == 'multiply':
@@ -145,7 +145,9 @@ class Kernels:
             return Kernels.linear(x1, x2) / Kernels.rbf(x1, x2, gamma)
         elif op == 'subtract':
             return np.abs(Kernels.linear(x1, x2) - Kernels.rbf(x1, x2, gamma))
-
+        elif op == 'dot':
+            return Kernels.linear(x1, x2).dot(10000*Kernels.rbf(x1, x2, gamma).T)
+        
     @staticmethod
     def rbfpoly(x1, x2, d = None, gamma = None, op = None):
         '''
@@ -165,7 +167,7 @@ class Kernels:
         else:
             d = d
         if not op:
-            op = 'add'
+            op = 'dot'
         else:
             op = op
         if op == 'multiply':
@@ -176,6 +178,8 @@ class Kernels:
             return Kernels.polynomial(x1, x2, d) / Kernels.rbf(x1, x2, gamma)
         elif op == 'subtract':
             return np.abs(Kernels.polynomial(x1, x2, d) - Kernels.rbf(x1, x2, gamma))
+        elif op == 'dot':
+            return Kernels.polynomial(x1, x2, d).dot(10000*Kernels.polynomial(x1, x2, d).T)
         
     @staticmethod
     def rbfcosine(x1, x2, gamma = None, op = None):
@@ -192,7 +196,7 @@ class Kernels:
         else:
             gamma = gamma
         if not op:
-            op = 'add'
+            op = 'dot'
         else:
             op = op
         if op == 'multiply':
@@ -203,7 +207,82 @@ class Kernels:
             return Kernels.cosine(x1, x2) / Kernels.rbf(x1, x2, gamma)
         elif op == 'subtract':
             return np.abs(Kernels.cosine(x1, x2) - Kernels.rbf(x1, x2, gamma))
+        elif op == 'dot':
+            return Kernels.cosine(x1, x2).dot(10000*Kernels.cosine(x1, x2).T)
         
+    @staticmethod
+    def etakernel(x1, x2, d = None, gamma = None, op = None):
+        '''
+        MKL: Pavlidis et al. (2001)
+        ----------------------------------------------
+        :param: x1: NxD transposed feature space
+        :param: x2: NxD feature space
+        :param: gamma: 1/2(sigma-square)
+        :return type: kernel(Gram) matrix
+        '''
+        if not gamma:
+            gamma = 1/x1.shape[1]
+        else:
+            gamma = gamma
+        if not op:
+            op = 'etapoly'
+        else:
+            op = op
+        if not d:
+            d = 3
+        else:
+            d = d
+        if op == 'eta':
+            return Kernels.linrbf(x1, x2).dot(Kernels.rbfpoly(x1, x2))
+        elif op == 'etapoly':
+            return Kernels.linrbf(x1, x2).dot(Kernels.rbfpoly(x1, x2)) + Kernels.rbfpoly(x1, x2).dot(Kernels.rbfcosine(x1, x2))
+        elif op == 'etasig':
+            return Kernels.sigmoid(x1, x2).dot(Kernels.rbf(x1, x2)) + Kernels.rbfpoly(x1, x2).dot(Kernels.sigmoid(x1, x2))
+        elif op == 'etaalpha':
+            return Kernels.rbf(Kernels.linear(x1, x2).dot(Kernels.rbfpoly(x1, x2)), Kernels.sigmoid(x1, x2) + Kernels.polynomial(x1, x2))
+            
+    @staticmethod
+    def alignment(x1, x2, d = None, gamma = None, op = None):
+        '''
+        MKL: Cortes et al.
+        ----------------------------------------------
+        :param: x1: NxD transposed feature space
+        :param: x2: NxD feature space
+        :param: gamma: 1/2(sigma-square)
+        :return type: kernel(Gram) matrix
+        '''
+        if not gamma:
+            gamma = 1/x1.shape[1]
+        else:
+            gamma = gamma
+        if not op:
+            op = 'rbfpoly'
+        else:
+            op = op
+        if not d:
+            d = 3
+        else:
+            d = d
         
-        
+        kappa_lin = Kernels.linear(x1, x2) - 1/len(x1)*np.ones((x1.shape[0], x1.shape[0])).dot(np.ones((x1.shape[0], x1.shape[0])).T).dot(Kernels.linear(x1, x2))\
+                     - 1/len(x1)*Kernels.linear(x1, x2).dot(np.ones((x1.shape[0], x1.shape[0])).dot(np.ones((x1.shape[0], x1.shape[0])).T)) + \
+                     1/len(x1)**2*(np.ones((x1.shape[0], x1.shape[0])).T.dot(Kernels.linear(x1, x2))).dot(np.ones((x1.shape[0], x1.shape[0]))).dot(np.ones((x1.shape[0], x1.shape[0])).dot(np.ones((x1.shape[0], x1.shape[0])).T))
+        kappa_rbf = Kernels.rbf(x1, x2) - 1/len(x1)*np.ones((x1.shape[0], x1.shape[0])).dot(np.ones((x1.shape[0], x1.shape[0])).T).dot(Kernels.rbf(x1, x2))\
+                     - 1/len(x1)*Kernels.rbf(x1, x2).dot(np.ones((x1.shape[0], x1.shape[0])).dot(np.ones((x1.shape[0], x1.shape[0])).T)) + \
+                     1/len(x1)**2*(np.ones((x1.shape[0], x1.shape[0])).T.dot(Kernels.rbf(x1, x2))).dot(np.ones((x1.shape[0], x1.shape[0]))).dot(np.ones((x1.shape[0], x1.shape[0])).dot(np.ones((x1.shape[0], x1.shape[0])).T))    
+        kappa_poly = Kernels.polynomial(x1, x2) - 1/len(x1)*np.ones((x1.shape[0], x1.shape[0])).dot(np.ones((x1.shape[0], x1.shape[0])).T).dot(Kernels.polynomial(x1, x2))\
+                     - 1/len(x1)*Kernels.polynomial(x1, x2).dot(np.ones((x1.shape[0], x1.shape[0])).dot(np.ones((x1.shape[0], x1.shape[0])).T)) + \
+                     1/len(x1)**2*(np.ones((x1.shape[0], x1.shape[0])).T.dot(Kernels.polynomial(x1, x2))).dot(np.ones((x1.shape[0], x1.shape[0]))).dot(np.ones((x1.shape[0], x1.shape[0])).dot(np.ones((x1.shape[0], x1.shape[0])).T))
+        kappa_sigmoid = Kernels.sigmoid(x1, x2) - 1/len(x1)*np.ones((x1.shape[0], x1.shape[0])).dot(np.ones((x1.shape[0], x1.shape[0])).T).dot(Kernels.sigmoid(x1, x2))\
+                     - 1/len(x1)*Kernels.sigmoid(x1, x2).dot(np.ones((x1.shape[0], x1.shape[0])).dot(np.ones((x1.shape[0], x1.shape[0])).T)) + \
+                     1/len(x1)**2*(np.ones((x1.shape[0], x1.shape[0])).T.dot(Kernels.sigmoid(x1, x2))).dot(np.ones((x1.shape[0], x1.shape[0]))).dot(np.ones((x1.shape[0], x1.shape[0])).dot(np.ones((x1.shape[0], x1.shape[0])).T))
+        if op == 'linrbf':
+            return kappa_lin.dot(kappa_rbf)/np.sqrt(np.sum(kappa_lin.dot(kappa_lin))*np.sum(kappa_rbf.dot(kappa_rbf)))
+        elif op == 'rbfpoly':
+            return kappa_rbf.dot(kappa_poly)/np.sqrt(np.sum(kappa_rbf.dot(kappa_rbf))*np.sum(kappa_poly.dot(kappa_poly)))
+        elif op == 'rbfsig':
+            return kappa_rbf.dot(kappa_sigmoid)/np.sqrt(np.sum(kappa_rbf.dot(kappa_rbf))*np.sum(kappa_sigmoid.dot(kappa_sigmoid)))
+        elif op == 'polysig':
+            return kappa_poly.dot(kappa_sigmoid)/np.sqrt(np.sum(kappa_poly.dot(kappa_poly))*np.sum(kappa_sigmoid.dot(kappa_sigmoid)))
+        return 
         
